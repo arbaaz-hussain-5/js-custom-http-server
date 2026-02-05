5a19f8f15f2a454616bbb9d1878b31a77d7d4ed7
Md Arbaaz Hussain <mdarbaazhussain666@gmain.com>
2026-02-05 11:54:46 +0530

    enhance the code

--- src/http-server.js
+++ src/http-server.js
@@ -1,21 +1,16 @@
-const http = require("http");
-const fs = require('fs').promises;
-
-
-const host = 'localhost';
-const port = 8000;
-
+import http from "http";
+import fs from 'fs/promises';
+const host =  process.env.HOST;
+const port = process.env.PORT;
 let indexFile;
-
-
 const requestListener = function (req, res) {
     res.setHeader("Content-Type", "text/html");
     res.writeHead(200);
+    console.log(res);
     res.end(indexFile);
 };
 
 const server = http.createServer(requestListener);
-
 
 fs.readFile("C:\\Users\\arbaa\\rproject\\hello_world\\http-server\\public\\html\\example1.html")
     .then(contents => {
